5a0427fa00b2465e47f9235683bd37e51b59cc27
TishoStefanov84 <tisho.stefanov@abv.bg>
2020-11-13 14:50:01 +0200

    Implement users

---
+++ Apps/SharedTrip/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedTrip.Services
+{
+    public interface IUserService
+    {
+        void Create(string username, string email, string password);
+
+        string GetUserId(string username, string password);
+
+        bool IsUsernameAvailable(string username);
+
+        bool IsEmailAvailable(string email);
+    }
+}
